e97990e645997d9477ab1558ae317f85d6aaaeaf
hanbing <1160531394@qq.com>
2025-03-23 18:26:26 +0800

    submit

---
+++ kgBuilder-ui/src/components/SvgIcon.vue
@@ -0,0 +1,33 @@
+<template>
+</template>
+<script>
+export default {
+  name: 'svg-icon',
+  props: {
+    size: {
+      type: [Number, String],
+      default: 16
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+    fill: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    ClassStyle () {
+      return `width:${parseInt(this.size)};height:${parseInt(this.size)};fill:${this.fill};`
+    },
+    IconName () {
+      return `#${this.icon}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
